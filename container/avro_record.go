011a1915b3937072c2e00f4daf567c857f15c7d5
Or Rikon <rikonor@gmail.com>
2017-02-16 15:51:37 -0500

    Add SendStats to AvroRecord interface (#9)

--- container/avro_record.go
+++ container/avro_record.go
@@ -2,9 +2,12 @@ package container
 
 import (
 	"io"
+
+	stats "github.com/securityscorecard/go-stats"
 )
 
 type AvroRecord interface {
 	Serialize(io.Writer) error
 	Schema() string
+	SendStats(stats.Statser)
 }
